d98920d45fb9c0ba6dabbb975eb38c8fba490957
kakaxi666 <an456789123@163.com>
2022-09-16 22:40:03 +0800

    更新代码

---
+++ src/components/MyHeader/index.vue
@@ -0,0 +1,48 @@
+<template>
+        <el-header>
+            <el-row :gutter="20" type="flex" justify="space-around" align="middle" style="border-bottom: 1px solid #e6e6e6;">
+                <el-col :span="4">
+                    <div class="word-font">张家的小五只</div>
+                </el-col>
+                <el-col :span="12"><div class="grid-content bg-purple">12313</div></el-col>
+                <el-col :span="8">
+                    <el-menu
+                            class="el-menu-demo"
+                            mode="horizontal"
+                            router
+                            background-color="transparent"
+                           >
+                        <el-menu-item index="home">home</el-menu-item>
+                        <el-menu-item index="search">search</el-menu-item>
+                    </el-menu>
+                </el-col>
+            </el-row>
+        </el-header>
+</template>
+<script>
+    export default{
+        name:"MyHeader",
+        data(){
+            return{
+                activeName:"/"
+            }
+        },
+        methods:{
+            handleClick(tab, event) {
+                this.$router.push({ name:this.activeName} )
+            }
+        }
+    }
+</script>
+<style lang="less" scoped>
+.word-font{
+    color:#303133
+}
+.el-header{
+    background-color: whitesmoke;
+    color: whitesmoke;
+    text-align: center;
+    /* line-height: 60px; */
+  }
+   
+</style>
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,30 +1,22 @@
 <template>
     <div>
         <el-container>
-            <el-header>
-                <el-row :gutter="20">
-                    <el-col :span="4"><div class="grid-content bg-purple">张家的小五只</div></el-col>
-                    <el-col :span="12"><div class="grid-content bg-purple">{{activeName}}</div></el-col>
-                    <el-col :span="8">
-                            <el-tabs v-model="activeName" @tab-click="handleClick">
-                                <el-tab-pane label="用户管理" name="/" ></el-tab-pane>
-                                <el-tab-pane label="角色管理" name="home">角色管理</el-tab-pane>
-                                <el-tab-pane label="配置管理" name="search">配置管理</el-tab-pane>
-                            </el-tabs>
-
-                    </el-col>
-                </el-row>
-            </el-header>
-            <el-main>Main</el-main>
+            <my-header></my-header>
+            <my-main></my-main>
             <el-footer>Footer</el-footer>
         </el-container>   
     </div>
 </template>
 <script>
 
+import MyHeader from '@/components/MyHeader'
+import MyMain from '@/components/MyMain' 
 export default{
     name: "Home",
-    
+    components:{
+        MyHeader,
+        MyMain
+    },
     data(){
         return{
             activeName:"/" 
@@ -38,7 +30,8 @@ export default{
 }
 </script>
 <style scoped>
-.el-header, .el-footer {
+
+.el-footer {
     background-color: #B3C0D1;
     color: whitesmoke;
     text-align: center;
@@ -53,16 +46,4 @@ export default{
     line-height: 300px;
   }
   
-  body > .el-container {
-    margin-bottom: 40px;
-  }
-  
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-  
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
 </style>
